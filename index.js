eb4c47a196246284eee44ef014bd43cdc44b443f
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-15 12:14:38 +0100

    removed previously created loop for of to create a promise all based solution

--- index.js
+++ index.js
@@ -4,21 +4,22 @@ const fs = require('fs')
 const util = require('util')
 
 // Method no2
-const lstat = util.promisify(fs.lstat)
+// const lstat = util.promisify(fs.lstat)
 
 //Method no.3 
-const { lstat } = fs.promises
+// const { lstat } = fs.promises
 
-fs.readdir(process.cwd(), (err, filenames) => {
+fs.readdir(process.cwd(), async (err, filenames) => {
 
   if (err) {
     console.log(err)
   }
 
+
 })
 
 // Method no.1
-//
+
 // const lstat = (filename) => {
 //   return new Promise((resolve, reject) => {
 //     fs.lstat(filename, (err, stats) => {
