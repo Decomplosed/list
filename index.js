951e56edc8aba0022f4b443d872626b903dcdf3a
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-15 11:57:26 +0100

    second method of using promises in that case

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 
 const fs = require('fs')
+const util = require('util')
+
+// Method no2
+const lstat = util.promisify(fs.lstat)
 
 fs.readdir(process.cwd(), (err, filenames) => {
 
@@ -10,14 +14,16 @@ fs.readdir(process.cwd(), (err, filenames) => {
 
 })
 
-const lstat = (filename) => {
-  return new Promise((resolve, reject) => {
-    fs.lstat(filename, (err, stats) => {
-      if (err) {
-        reject(err)
-      }
+// Method no.1
+//
+// const lstat = (filename) => {
+//   return new Promise((resolve, reject) => {
+//     fs.lstat(filename, (err, stats) => {
+//       if (err) {
+//         reject(err)
+//       }
 
-      resolve(stats)
-    })
-  })
-}
+//       resolve(stats)
+//     })
+//   })
+// }
